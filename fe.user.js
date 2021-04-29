e44fbe9a155747d129640aad9484d7e8164d56c4
shadow-boop <83372311+shadow-boop@users.noreply.github.com>
2021-04-29 16:35:28 +0100

    Update fe.user.js

--- fe.user.js
+++ fe.user.js
@@ -11,49 +11,60 @@
 
 (function () {
     'use strict';
-    setTimeout(function () {
-        function decodeHTMLEntities(text) {
-            var entities = [
-                ['amp', '&'],
-                ['apos', '\''],
-                ['#x27', '\''],
-                ['#x2F', '/'],
-                ['#39', '\''],
-                ['#47', '/'],
-                ['lt', '<'],
-                ['gt', '>'],
-                ['nbsp', ' '],
-                ['quot', '"']
-            ];
-
-            for (var i = 0, max = entities.length; i < max; ++i) {
-                text = text.replace(new RegExp('&' + entities[i][0] + ';', 'g'), entities[i][1]);
-            }
-            text = text.replace("Â&nbsp;"," ")
-            return text;
-        }
-        var contentBody = document.getElementById('sfContentBody')
-        var text = ""
-        contentBody.childNodes.forEach(content => {
-            var add = content.textContent
-
-            text += `${decodeHTMLEntities(add)}\n`
-        })
-        var data = new Blob([text], {
-            type: 'application/octet-stream'
-        });
-        data.name = document.getElementById("sfContentTitle").textContent
-        var url = window.URL.createObjectURL(data);
-        var downloadbutton = document.createElement("a")
-        downloadbutton.style.position = "absolute"
-        downloadbutton.style.top = "150px"
-        downloadbutton.style.left = "200px"
-        downloadbutton.href = url;
-        downloadbutton.style.zIndex = 100000;
-        downloadbutton.textContent = "download"
-        downloadbutton.download = document.getElementById("sfContentTitle").textContent + ".txt"
-        contentBody.appendChild(downloadbutton)
-        console.log(url)
-    }, 500)
-
+    var lsplit = window.location.pathname.split('/')
+    if(lsplit[lsplit.length - 1].includes('.')){
+        lsplit.pop()
+    }
+    var location = window.location.hostname + lsplit.join('/')
+    switch(location){
+        case "sofurry.com/view/":
+            setTimeout(function () {
+                function decodeHTMLEntities(text) {
+                    var entities = [
+                        ['amp', '&'],
+                        ['apos', '\''],
+                        ['#x27', '\''],
+                        ['#x2F', '/'],
+                        ['#39', '\''],
+                        ['#47', '/'],
+                        ['lt', '<'],
+                        ['gt', '>'],
+                        ['nbsp', ' '],
+                        ['quot', '"']
+                    ];
+        
+                    for (var i = 0, max = entities.length; i < max; ++i) {
+                        text = text.replace(new RegExp('&' + entities[i][0] + ';', 'g'), entities[i][1]);
+                    }
+                    text = text.replace("Â&nbsp;"," ");
+                    return text;
+                }
+                var contentBody = document.getElementById('sfContentBody');
+                var text = "";
+                contentBody.childNodes.forEach(content => {
+                    var add = content.textContent;
+        
+                    text += `${decodeHTMLEntities(add)}\n`;
+                })
+                var data = new Blob([text], {
+                    type: 'application/octet-stream'
+                });
+                data.name = document.getElementById("sfContentTitle").textContent;
+                var url = window.URL.createObjectURL(data);
+                var downloadbutton = document.createElement("a");
+                downloadbutton.style.position = "absolute";
+                downloadbutton.style.top = "150px";
+                downloadbutton.style.left = "200px";
+                downloadbutton.href = url;
+                downloadbutton.style.zIndex = 100000;
+                downloadbutton.textContent = "download";
+                downloadbutton.download = document.getElementById("sfContentTitle").textContent + ".txt";
+                contentBody.appendChild(downloadbutton);
+                console.log(url);
+            }, 500);
+            break;
+        default:
+            console.log(location)
+            break;
+    }
 })();
